a2f90141ebc40f0248c407ac573b04da73ea60cd
Iekna Lora <ieknalora@gmail.com>
2026-05-13 11:47:37 -0400

    creaacion de la clase candidatos

---
+++ Entities/Candidato.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemadeVotaciones.Entities
+{
+    public class Candidato
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Puesto { get; set; }
+        public string Curso { get; set; }
+        public string Foto { get; set; }
+        public int PlanchaId { get; set; }
+    }
+}
