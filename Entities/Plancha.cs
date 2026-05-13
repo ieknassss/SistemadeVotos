c9963e0e351392e6309118af6406f9933663f171
Iekna Lora <ieknalora@gmail.com>
2026-05-13 11:46:58 -0400

    creacion de la clase planchas

---
+++ Entities/Plancha.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemadeVotaciones.Entities
+{
+    public class Plancha
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Siglas { get; set; }
+        public string Logo { get; set; }
+        public int PadronId { get; set; }
+    }
+}
--- Form/FrmMenuPrincipal.Designer.cs
+++ Form/FrmMenuPrincipal.Designer.cs
@@ -33,6 +33,7 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenuPrincipal));
             this.panelMenu = new System.Windows.Forms.Panel();
             this.btnCerrarSesion = new System.Windows.Forms.Button();
             this.btnResultados = new System.Windows.Forms.Button();
@@ -42,20 +43,21 @@
             this.btnVotar = new System.Windows.Forms.Button();
             this.btnPlanchas = new System.Windows.Forms.Button();
             this.btnUsuarios = new System.Windows.Forms.Button();
-
             this.panelTop = new System.Windows.Forms.Panel();
             this.lblRol = new System.Windows.Forms.Label();
             this.lblUsuario = new System.Windows.Forms.Label();
             this.lblTitulo = new System.Windows.Forms.Label();
-
             this.panelContenedor = new System.Windows.Forms.Panel();
-
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelMenu.SuspendLayout();
             this.panelTop.SuspendLayout();
+            this.panelContenedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-
+            // 
             // panelMenu
-            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.panelMenu.Controls.Add(this.btnCerrarSesion);
             this.panelMenu.Controls.Add(this.btnResultados);
             this.panelMenu.Controls.Add(this.btnReportes);
@@ -66,189 +68,233 @@
             this.panelMenu.Controls.Add(this.btnUsuarios);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(220, 650);
+            this.panelMenu.Size = new System.Drawing.Size(330, 1000);
             this.panelMenu.TabIndex = 0;
-
+            // 
             // btnCerrarSesion
-            this.btnCerrarSesion.BackColor = System.Drawing.Color.FromArgb(192, 57, 43);
+            // 
+            this.btnCerrarSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
             this.btnCerrarSesion.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.btnCerrarSesion.FlatAppearance.BorderSize = 0;
             this.btnCerrarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCerrarSesion.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.btnCerrarSesion.ForeColor = System.Drawing.Color.White;
-            this.btnCerrarSesion.Location = new System.Drawing.Point(0, 605);
+            this.btnCerrarSesion.Location = new System.Drawing.Point(0, 931);
+            this.btnCerrarSesion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCerrarSesion.Name = "btnCerrarSesion";
-            this.btnCerrarSesion.Size = new System.Drawing.Size(220, 45);
+            this.btnCerrarSesion.Size = new System.Drawing.Size(330, 69);
             this.btnCerrarSesion.TabIndex = 7;
             this.btnCerrarSesion.Text = "Cerrar Sesión";
             this.btnCerrarSesion.UseVisualStyleBackColor = false;
             this.btnCerrarSesion.Click += new System.EventHandler(this.btnCerrarSesion_Click);
-
+            // 
             // btnResultados
-            this.btnResultados.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            // 
+            this.btnResultados.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.btnResultados.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnResultados.FlatAppearance.BorderSize = 0;
             this.btnResultados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnResultados.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.btnResultados.ForeColor = System.Drawing.Color.White;
-            this.btnResultados.Location = new System.Drawing.Point(0, 270);
+            this.btnResultados.Location = new System.Drawing.Point(0, 414);
+            this.btnResultados.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnResultados.Name = "btnResultados";
-            this.btnResultados.Size = new System.Drawing.Size(220, 45);
+            this.btnResultados.Size = new System.Drawing.Size(330, 69);
             this.btnResultados.TabIndex = 6;
             this.btnResultados.Text = "Resultados";
             this.btnResultados.UseVisualStyleBackColor = false;
-
+            // 
             // btnReportes
-            this.btnReportes.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            // 
+            this.btnReportes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.btnReportes.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnReportes.FlatAppearance.BorderSize = 0;
             this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnReportes.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.btnReportes.ForeColor = System.Drawing.Color.White;
-            this.btnReportes.Location = new System.Drawing.Point(0, 225);
+            this.btnReportes.Location = new System.Drawing.Point(0, 345);
+            this.btnReportes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnReportes.Name = "btnReportes";
-            this.btnReportes.Size = new System.Drawing.Size(220, 45);
+            this.btnReportes.Size = new System.Drawing.Size(330, 69);
             this.btnReportes.TabIndex = 5;
             this.btnReportes.Text = "Reportes";
             this.btnReportes.UseVisualStyleBackColor = false;
-
+            // 
             // btnPanel
-            this.btnPanel.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            // 
+            this.btnPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.btnPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnPanel.FlatAppearance.BorderSize = 0;
             this.btnPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPanel.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.btnPanel.ForeColor = System.Drawing.Color.White;
-            this.btnPanel.Location = new System.Drawing.Point(0, 180);
+            this.btnPanel.Location = new System.Drawing.Point(0, 276);
+            this.btnPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnPanel.Name = "btnPanel";
-            this.btnPanel.Size = new System.Drawing.Size(220, 45);
+            this.btnPanel.Size = new System.Drawing.Size(330, 69);
             this.btnPanel.TabIndex = 4;
             this.btnPanel.Text = "Panel Electoral";
             this.btnPanel.UseVisualStyleBackColor = false;
-
+            // 
             // btnDatos
-            this.btnDatos.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            // 
+            this.btnDatos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.btnDatos.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnDatos.FlatAppearance.BorderSize = 0;
             this.btnDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDatos.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.btnDatos.ForeColor = System.Drawing.Color.White;
-            this.btnDatos.Location = new System.Drawing.Point(0, 135);
+            this.btnDatos.Location = new System.Drawing.Point(0, 207);
+            this.btnDatos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnDatos.Name = "btnDatos";
-            this.btnDatos.Size = new System.Drawing.Size(220, 45);
+            this.btnDatos.Size = new System.Drawing.Size(330, 69);
             this.btnDatos.TabIndex = 3;
             this.btnDatos.Text = "Datos Generales";
             this.btnDatos.UseVisualStyleBackColor = false;
-
+            // 
             // btnVotar
-            this.btnVotar.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            // 
+            this.btnVotar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.btnVotar.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnVotar.FlatAppearance.BorderSize = 0;
             this.btnVotar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVotar.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.btnVotar.ForeColor = System.Drawing.Color.White;
-            this.btnVotar.Location = new System.Drawing.Point(0, 90);
+            this.btnVotar.Location = new System.Drawing.Point(0, 138);
+            this.btnVotar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnVotar.Name = "btnVotar";
-            this.btnVotar.Size = new System.Drawing.Size(220, 45);
+            this.btnVotar.Size = new System.Drawing.Size(330, 69);
             this.btnVotar.TabIndex = 2;
             this.btnVotar.Text = "Votar";
             this.btnVotar.UseVisualStyleBackColor = false;
-
+            // 
             // btnPlanchas
-            this.btnPlanchas.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            // 
+            this.btnPlanchas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.btnPlanchas.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnPlanchas.FlatAppearance.BorderSize = 0;
             this.btnPlanchas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPlanchas.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.btnPlanchas.ForeColor = System.Drawing.Color.White;
-            this.btnPlanchas.Location = new System.Drawing.Point(0, 45);
+            this.btnPlanchas.Location = new System.Drawing.Point(0, 69);
+            this.btnPlanchas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnPlanchas.Name = "btnPlanchas";
-            this.btnPlanchas.Size = new System.Drawing.Size(220, 45);
+            this.btnPlanchas.Size = new System.Drawing.Size(330, 69);
             this.btnPlanchas.TabIndex = 1;
             this.btnPlanchas.Text = "Planchas";
             this.btnPlanchas.UseVisualStyleBackColor = false;
-
+            // 
             // btnUsuarios
-            this.btnUsuarios.BackColor = System.Drawing.Color.FromArgb(44, 62, 80);
+            // 
+            this.btnUsuarios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.btnUsuarios.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnUsuarios.FlatAppearance.BorderSize = 0;
             this.btnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUsuarios.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
             this.btnUsuarios.ForeColor = System.Drawing.Color.White;
             this.btnUsuarios.Location = new System.Drawing.Point(0, 0);
+            this.btnUsuarios.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnUsuarios.Name = "btnUsuarios";
-            this.btnUsuarios.Size = new System.Drawing.Size(220, 45);
+            this.btnUsuarios.Size = new System.Drawing.Size(330, 69);
             this.btnUsuarios.TabIndex = 0;
             this.btnUsuarios.Text = "Usuarios";
             this.btnUsuarios.UseVisualStyleBackColor = false;
             this.btnUsuarios.Click += new System.EventHandler(this.btnUsuarios_Click);
-
+            // 
             // panelTop
-            this.panelTop.BackColor = System.Drawing.Color.FromArgb(52, 73, 94);
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
             this.panelTop.Controls.Add(this.lblRol);
             this.panelTop.Controls.Add(this.lblUsuario);
             this.panelTop.Controls.Add(this.lblTitulo);
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Location = new System.Drawing.Point(220, 0);
+            this.panelTop.Location = new System.Drawing.Point(330, 0);
+            this.panelTop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(880, 70);
+            this.panelTop.Size = new System.Drawing.Size(1320, 108);
             this.panelTop.TabIndex = 1;
-
-            // lblTitulo
-            this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
-            this.lblTitulo.ForeColor = System.Drawing.Color.White;
-            this.lblTitulo.Location = new System.Drawing.Point(25, 18);
-            this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(356, 32);
-            this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "Sistema de Votaciones Escolar";
-
-            // lblUsuario
-            this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            this.lblUsuario.ForeColor = System.Drawing.Color.White;
-            this.lblUsuario.Location = new System.Drawing.Point(610, 14);
-            this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(65, 19);
-            this.lblUsuario.TabIndex = 1;
-            this.lblUsuario.Text = "Usuario:";
-
+            // 
             // lblRol
+            // 
             this.lblRol.AutoSize = true;
             this.lblRol.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.lblRol.ForeColor = System.Drawing.Color.White;
-            this.lblRol.Location = new System.Drawing.Point(610, 40);
+            this.lblRol.Location = new System.Drawing.Point(915, 62);
+            this.lblRol.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRol.Name = "lblRol";
-            this.lblRol.Size = new System.Drawing.Size(35, 19);
+            this.lblRol.Size = new System.Drawing.Size(48, 28);
             this.lblRol.TabIndex = 2;
             this.lblRol.Text = "Rol:";
-
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblUsuario.ForeColor = System.Drawing.Color.White;
+            this.lblUsuario.Location = new System.Drawing.Point(915, 22);
+            this.lblUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(89, 28);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(38, 28);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(522, 48);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Sistema de Votaciones Escolar";
+            // 
             // panelContenedor
+            // 
             this.panelContenedor.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelContenedor.Controls.Add(this.pictureBox1);
             this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContenedor.Location = new System.Drawing.Point(220, 70);
+            this.panelContenedor.Location = new System.Drawing.Point(330, 108);
+            this.panelContenedor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelContenedor.Name = "panelContenedor";
-            this.panelContenedor.Size = new System.Drawing.Size(880, 580);
+            this.panelContenedor.Size = new System.Drawing.Size(1320, 892);
             this.panelContenedor.TabIndex = 2;
-
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(437, 139);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 312);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // FrmMenuPrincipal
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 650);
+            this.ClientSize = new System.Drawing.Size(1650, 1000);
             this.Controls.Add(this.panelContenedor);
             this.Controls.Add(this.panelTop);
             this.Controls.Add(this.panelMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmMenuPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menú Principal";
             this.Load += new System.EventHandler(this.FrmMenuPrincipal_Load);
-
             this.panelMenu.ResumeLayout(false);
             this.panelTop.ResumeLayout(false);
             this.panelTop.PerformLayout();
+            this.panelContenedor.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+
         }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
